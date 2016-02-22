020abfad7d964fed8a6dc1837777ce369080a2c9
Nidaa Bugis <bent_al7ejaz90@hotmail.com>
2016-02-21 19:34:30 -0800

    added git repo to oackaje.js

--- js/package.js
+++ js/package.js
@@ -2,6 +2,7 @@
   "name": "TRIZ",
   "version": "1.0.0",
   "description": "Online Form for TRIZ course",
+  "main": "app.js",
   "dependencies": {
   },
   "devDependencies": {
@@ -10,6 +11,10 @@
   "scripts": {
     "test": "echo \"Error: no test specified\" && exit 1"
   },
+  "repository":{
+    "type": "git",
+    "url": "https://github.com/nbogis/TRIZ.git"
+  }
   "author": "Nidaa Bugis",
   "license": "ISC"
 }
